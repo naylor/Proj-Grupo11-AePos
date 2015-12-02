c8828d9121721213627669b599492ee2c6c7cfc5
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 17:38:21 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -61,14 +61,14 @@ void box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMBlock* 
     const int widthStep = imageParams->coluna;
     const int filterWidth = 5;
     const int filterHeight = 5;
+
+    unsigned char CPUinput[width*height];
+    unsigned char CPUoutput[width*linhasOut];
+
     printf("Apply Smooth[%d][%s] - li:%d, lf:%d\n",
                numBlock, imageParams->tipo, block[numBlock].li, block[numBlock].lf);
 
     exit(1);
-    unsigned char CPUinput[width*height];
-    unsigned char CPUoutput[width*linhasOut];
-
-
 
     for(int t=0; t<width*height; t++)
         CPUinput[t] = block[numBlock].pgmIn[t].gray;
