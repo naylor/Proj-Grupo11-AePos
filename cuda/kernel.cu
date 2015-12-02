eb5c2ba0af52a7bd4d63e390c336121e610b06db
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 02:55:41 -0200

    Correcoes

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -74,7 +74,12 @@ __global__ void smoothPPM_SH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, in
     unsigned int shX = threadIdx.x + 2;
 
     // POPULANDO O BLOCO 20X20 (4X4 BORDA)
-    sharedMem[shY][shX] = kInput[offset];
+    for(int l = -2; l <= BLOCK_DIM+2; ++l) {
+        for(int c = -2; c <= BLOCK_DIM+2; ++c) {
+            const int p = (l+offset)+c;
+            sharedMem[shY+l][shX+c] = kInput[p];
+        }
+    }
     // SINCRONIZANDO AS THREADS
     __syncthreads();
 
