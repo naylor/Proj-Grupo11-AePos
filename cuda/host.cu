78e16c3927f3fb744064cb88c4a4a172f1988f73
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 16:50:01 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -8,113 +8,6 @@
 #define BLOCK_DIM 32
 #define BLOCK_DEFAULT 512
 
-texture<unsigned char, cudaTextureType2D> tex8u;
-
-
-// FUNCAO PARA APLICAR SMOOTH
-// COM SHARED MEMORY EM IMAGENS PPM
-__global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, const int height, const size_t pitch, const int fWidth, const int fHeight)
-{
-    int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-    int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-
-    printf("Apply Smooth[%d] ", tex2D(tex8u,xIndex,yIndex) );
-
-
-}
-
-void box_filter_8u_c1(PPMImageParams* imageParams, PPMBlock* block, int numBlock)
-{
-
-
-        const int widthStep=2;
-        const int filterWidth=5;
-        const int filterHeight=5;
-
-        const int width = imageParams->coluna;
-        const int height = block[numBlock].lf-block[numBlock].li;
-        printf("Apply Smooth[%d] ", height );
-
-        //CPUinput = (unsigned char *)malloc(imageParams->coluna * imageParams->linha * sizeof(unsigned char));
-        //CPUoutput = (unsigned char *)malloc(imageParams->coluna * imageParams->linha * sizeof(unsigned char));
-
-        unsigned char CPUinput[imageParams->coluna * (block[numBlock].lf-block[numBlock].li)];
-        unsigned char CPUoutput[imageParams->coluna * (block[numBlock].lf-block[numBlock].li)];
-
-
-
-        for(int t=0; t<imageParams->coluna*(block[numBlock].lf-block[numBlock].li); t++) {
-                CPUinput[t] =  block[numBlock].pgmIn[t].gray;
-        }
-
-
-
-    /*
-     * 2D memory is allocated as strided linear memory on GPU.
-     * The terminologies "Pitch", "WidthStep", and "Stride" are exactly the same thing.
-     * It is the size of a row in bytes.
-     * It is not necessary that width = widthStep.
-     * Total bytes occupied by the image = widthStep x height.
-     */
-
-    //Declare GPU pointer
-    unsigned char *GPU_input, *GPU_output;
-
-    //Allocate 2D memory on GPU. Also known as Pitch Linear Memory
-    size_t gpu_image_pitch = 0;
-    cudaMallocPitch<unsigned char>(&GPU_input,&gpu_image_pitch,width,height);
-    cudaMallocPitch<unsigned char>(&GPU_output,&gpu_image_pitch,width,height);
-
-    //Copy data from host to device.
-    cudaMemcpy2D(GPU_input,gpu_image_pitch,CPUinput,widthStep,width,height,cudaMemcpyHostToDevice);
-
-    //Bind the image to the texture. Now the kernel will read the input image through the texture cache.
-    //Use tex2D function to read the image
-    cudaBindTexture2D(NULL,tex8u,GPU_input,width,height,gpu_image_pitch);
-
-    /*
-     * Set the behavior of tex2D for out-of-range image reads.
-     * cudaAddressModeBorder = Read Zero
-     * cudaAddressModeClamp  = Read the nearest border pixel
-     * We can skip this step. The default mode is Clamp.
-     */
-    tex8u.addressMode[0] = tex8u.addressMode[1] = cudaAddressModeBorder;
-
-    /*
-     * Specify a block size. 256 threads per block are sufficient.
-     * It can be increased, but keep in mind the limitations of the GPU.
-     * Older GPUs allow maximum 512 threads per block.
-     * Current GPUs allow maximum 1024 threads per block
-     */
-
-    dim3 block_size(16,16);
-
-    /*
-     * Specify the grid size for the GPU.
-     * Make it generalized, so that the size of grid changes according to the input image size
-     */
-
-    dim3 grid_size;
-    grid_size.x = (width + block_size.x - 1)/block_size.x;  /*< Greater than or equal to image width */
-    grid_size.y = (height + block_size.y - 1)/block_size.y; /*< Greater than or equal to image height */
-
-    //Launch the kernel
-    box_filter_kernel_8u_c1<<<grid_size,block_size>>>(GPU_output,width,height,gpu_image_pitch,filterWidth,filterHeight);
-
-    //Copy the results back to CPU
-    cudaMemcpy2D(CPUoutput,widthStep,GPU_output,gpu_image_pitch,width,height,cudaMemcpyDeviceToHost);
-
-            for(int t=0; t<imageParams->coluna*(block[numBlock].lf-block[numBlock].li); t++)
-                   block[numBlock].pgmIn[t].gray = CPUoutput[t];
-
-    //Release the texture
-    cudaUnbindTexture(tex8u);
-
-    //Free GPU memory
-    cudaFree(GPU_input);
-    cudaFree(GPU_output);
-}
-
 // FUNCAO __HOST__
 // DEFINICAO DOS PARAMETROS DE CHAMADA DO KERNEL
 void applySmooth(initialParams* ct, PPMImageParams* imageParams, PPMBlock* block, int numBlock, cudaStream_t* streamSmooth) {
@@ -142,42 +35,15 @@ void applySmooth(initialParams* ct, PPMImageParams* imageParams, PPMBlock* block
         // SE A OPCAO DE SHARED MEMORY
         // FOR ATIVADA, DEFINE O TAMANHO
         // DO BLOCO PARA 32
-        //if (ct->sharedMemory == 1)
-        //    blockDims.x = BLOCK_DIM;
+        if (ct->sharedMemory == 1)
+            blockDims.x = BLOCK_DIM;
         dim3 gridDims((unsigned int) ceil((double)(linhasIn/blockDims.x)), 1, 1 );
-
-            //Declare GPU pointer
-            unsigned char *GPU_input, *GPU_output;
-            //Allocate 2D memory on GPU. Also known as Pitch Linear Memory
-            size_t gpu_image_pitch = 0;
-
-        dim3 block_size(16,16);
-dim3 grid_size;
-    grid_size.x = (imageParams->coluna + block_size.x - 1)/block_size.x;  /*< Greater than or equal to image width */
-    grid_size.y = (imageParams->linha + block_size.y - 1)/block_size.y; /*< Greater than or equal to image height */
 
         // EXECUTA O CUDAMEMCPY
         // ASSINCRONO OU SINCRONO
-        if (ct->async == 1) {
-
-            cudaMallocPitch<unsigned char>(&GPU_input,&gpu_image_pitch,imageParams->coluna,imageParams->linha);
-            cudaMallocPitch<unsigned char>(&GPU_output,&gpu_image_pitch,imageParams->coluna,imageParams->linha);
-
-            //Copy data from host to device.
-            cudaMemcpy2D(GPU_input,gpu_image_pitch,block[numBlock].ppmIn,imageParams->coluna,imageParams->coluna,imageParams->linha,cudaMemcpyHostToDevice);
-
-            //Bind the image to the texture. Now the kernel will read the input image through the texture cache.
-            //Use tex2D function to read the image
-            cudaBindTexture2D(NULL,tex8u,GPU_input,imageParams->coluna,imageParams->linha,gpu_image_pitch);
-
-            /*
-             * Set the behavior of tex2D for out-of-range image reads.
-             * cudaAddressModeBorder = Read Zero
-             * cudaAddressModeClamp  = Read the nearest border pixel
-             * We can skip this step. The default mode is Clamp.
-             */
-            tex8u.addressMode[0] = tex8u.addressMode[1] = cudaAddressModeBorder;
-        } else
+        if (ct->async == 1)
+            cudaMemcpyAsync( kInput, block[numBlock].ppmIn, linhasIn, cudaMemcpyHostToDevice, streamSmooth[numBlock] );
+        else
             cudaMemcpy( kInput, block[numBlock].ppmIn, linhasIn, cudaMemcpyHostToDevice);
 
         // EXECUTA A FUNCAO SMOOTH NO KERNEL
@@ -185,12 +51,12 @@ dim3 grid_size;
         // CHAMA A FUNCAO smoothPPM_SH
         if (ct->async == 1) {
             if (ct->sharedMemory == 1)
-                smoothPPM_noSH<<<gridDims, blockDims, 0, streamSmooth[numBlock]>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
+                smoothPPM_SH<<<gridDims, blockDims, 0, streamSmooth[numBlock]>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
             else
                 smoothPPM_noSH<<<gridDims, blockDims, 0, streamSmooth[numBlock]>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
         } else {
             if (ct->sharedMemory == 1)
-                smoothPPM_noSH<<<gridDims, blockDims>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
+                smoothPPM_SH<<<gridDims, blockDims>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
             else
                 smoothPPM_noSH<<<gridDims, blockDims>>>(kInput, kOutput, imageParams->coluna, imageParams->linha, block[numBlock].li, block[numBlock].lf);
         }
@@ -198,13 +64,9 @@ dim3 grid_size;
         // RETORNA A IMAGEM PARA
         // A VARIAVEL DE SAIDA PARA
         // GRAVACAO NO ARQUIVO
-        if (ct->async == 1) {
-            //Copy the results back to CPU
-            cudaMemcpy2D(block[numBlock].ppmOut,imageParams->coluna,GPU_output,gpu_image_pitch,imageParams->coluna,imageParams->linha,cudaMemcpyDeviceToHost);
-
-            //Release the texture
-            cudaUnbindTexture(tex8u);
-        } else
+        if (ct->async == 1)
+            cudaMemcpyAsync(block[numBlock].ppmOut, kOutput, linhasOut, cudaMemcpyDeviceToHost, streamSmooth[numBlock] );
+        else
             cudaMemcpy(block[numBlock].ppmOut, kOutput, linhasOut, cudaMemcpyDeviceToHost );
 
         // LIBERA A MEMORIA
--- cuda/kernel.cuh
+++ cuda/kernel.cuh
@@ -5,7 +5,7 @@
 
 __global__ void smoothPGM_noSH(PGMPixel* kInput, PGMPixel* kOutput, int coluna, int linha, int li, int lf);
 __global__ void smoothPPM_noSH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, int linha, int li, int lf);
-//__global__ void smoothPPM_SH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, int linha, int li, int lf);
+__global__ void smoothPPM_SH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, int linha, int li, int lf);
 __global__ void smoothPGM_SH(PGMPixel* kInput, PGMPixel* kOutput, int coluna, int linha, int li, int lf);
 
 
