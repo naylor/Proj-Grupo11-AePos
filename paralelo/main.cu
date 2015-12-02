1224e9e6b7e92a2454de16941229dfce5298494a
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 16:20:06 -0200

    Correcoes

--- paralelo/main.cu
+++ paralelo/main.cu
@@ -93,22 +93,22 @@ int main(int argc, char** argv) {
         i++;
     }
 
-    for(int t=0; t<i; t++) {
+    //for(int t=0; t<i; t++) {
         // FAZ A LEITURA DA PARTE DA IMAGEM
         // NO DISCO
         start_timer(tempoR); //INICIA O RELOGIO
-        getImageBlocks(ct, imageParams, block,  t);
+        getImageBlocks(ct, imageParams, block,  0);
         stop_timer(tempoR);
 
         //applySmooth(ct, imageParams, block, t, streamSmooth);
-        box_filter_8u_c1(imageParams, block, t);
+        box_filter_8u_c1(imageParams, block, 0);
 
 
         // FAZ A GRAVACAO
         start_timer(tempoW); //INICIA O RELOGIO
-        writePPMPixels(ct, imageParams, block, t);
+        writePPMPixels(ct, imageParams, block, 0);
         stop_timer(tempoW);
-    }
+   // }
 
     //PARA O RELOGIO
     show_timer(tempoR, "READ");
