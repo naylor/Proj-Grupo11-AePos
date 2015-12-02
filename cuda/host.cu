5b7601b7e0d4f624046e8acb982bbd8314f6e0fc
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 17:47:42 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -51,25 +51,19 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
 void box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMBlock* block, int numBlock, cudaStream_t* streamSmooth)
 {
 
-    int linhasIn = block[numBlock].linhasIn;
-    int linhasOut = block[numBlock].linhasOut;
+    double linhasIn = block[numBlock].linhasIn;
+    double linhasOut = block[numBlock].linhasOut;
 
-
-    const int width = imageParams->linha;
-    const int height = imageParams->coluna;
-
-    const int widthStep = imageParams->linha;
+    const int width = imageParams->coluna;
+    const int height = block[numBlock].lf-block[numBlock].li;
+    const int widthStep = imageParams->coluna;
     const int filterWidth = 5;
     const int filterHeight = 5;
 
+    unsigned char CPUinput[width*width];
+    unsigned char CPUoutput[width*width];
 
-
-    unsigned char CPUinput[width*height];
-    unsigned char CPUoutput[width*height];
-
-
-
-    for(int t=0; t<width*height; t++)
+    for(int t=0; t<width*width; t++)
         CPUinput[t] = block[numBlock].pgmIn[t].gray;
 
 
@@ -128,7 +122,7 @@ void box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMBlock* 
     //Copy the results back to CPU
     cudaMemcpy2D(CPUoutput,widthStep,GPU_output,gpu_image_pitch,width,height,cudaMemcpyDeviceToHost);
 
-    for(int t=0; t<width*linhasOut; t++)
+    for(int t=0; t<width*width; t++)
         block[numBlock].pgmOut[t].gray = CPUoutput[t];
 
     //Release the texture
