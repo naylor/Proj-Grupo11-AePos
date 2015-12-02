f3f26ade82f66a60de1b8fc0be6728557d520575
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-01 23:23:41 -0200

    Correcoes

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -74,15 +74,15 @@ __global__ void smoothPPM_SH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, in
     unsigned int shX = threadIdx.x + 2;
 
     // POPULANDO O BLOCO 20X20 (4X4 BORDA)
-    if (threadIdx.x==0 || threadIdx.x==BLOCK_DIM-2 || threadIdx.y==0 || threadIdx.y==BLOCK_DIM-2){
+    if (threadIdx.x==0 || threadIdx.x==BLOCK_DIM-1 || threadIdx.y==0 || threadIdx.y==BLOCK_DIM-1){
         for(int l = -2; l < 0; ++l) {
             for(int c = -2; c < 0; ++c) {
                 const int p = (l+offset)+c;
                 sharedMem[shY+l][shX+c] = kInput[p];
             }
         }
-        for(int l = BLOCK_DIM; l < BLOCK_DIM+2; ++l) {
-            for(int c = BLOCK_DIM; c < BLOCK_DIM+2; ++c) {
+        for(int l = BLOCK_DIM; l <= BLOCK_DIM+2; ++l) {
+            for(int c = BLOCK_DIM; c <= BLOCK_DIM+2; ++c) {
                 const int p = (l+offset)+c;
                 sharedMem[shY+l][shX+c] = kInput[p];
             }
