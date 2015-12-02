c269a24838274fefb42c95d3f54187a84256d96b
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-02 01:50:40 -0200

    Correcoes

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -74,8 +74,8 @@ __global__ void smoothPPM_SH(PPMPixel* kInput, PPMPixel* kOutput, int coluna, in
     unsigned int shX = threadIdx.x + 2;
 
     // POPULANDO O BLOCO 20X20 (4X4 BORDA)
-    if (((threadIdx.x >= 0 && threadIdx.x < 2) || threadIdx.x > BLOCK_DIM-2) || (threadIdx.y >= 0 && threadIdx.y < 2)) {
-        sharedMem[threadIdx.y][threadIdx.x+c] = kInput[offset];
+    if (threadIdx.y >= 0 && threadIdx.y < 2) {
+        sharedMem[shY][shX] = kInput[offset];
     }
     else
         sharedMem[shY][shX] = kInput[offset];
