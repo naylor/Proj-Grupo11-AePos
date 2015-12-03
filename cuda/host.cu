9879bc706855d259c65114ce4ef2408bb99710e5
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-03 01:33:10 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -26,7 +26,8 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     int l = (xIndex-c)/width; // LINHA
 
 
-    if ( l > lf-li || c < 2 || c > width-2 || (li == 0 && yIndex < 2) || (lf==height-1 && yIndex > (lf-li)-2) )
+    // TIRANDO A BORDA DO PROCESSAMENTO
+    if ( l > lf-li || c < 2 || c > width-2 || (li == 0 && l < 2) || (lf==height-1 && l > (lf-li)-2) )
         return;
 
     int inicio = xIndex;
