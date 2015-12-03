117fe607ceb6b8f86f270fb284a4d5a29fdd4b3c
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-03 00:52:39 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -96,11 +96,11 @@ void box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMBlock* 
     cudaMallocPitch<unsigned char>(&GPU_output,&gpu_image_pitch,width,height);
 
     //Copy data from host to device.
-    cudaMemcpy2D(GPU_input,gpu_image_pitch,CPUinput,widthStep,width,height,cudaMemcpyHostToDevice);
+    cudaMemcpy2D(GPU_input,gpu_image_pitch,CPUinput,widthStep,width,block[numBlock].linhas,cudaMemcpyHostToDevice);
 
     //Bind the image to the texture. Now the kernel will read the input image through the texture cache.
     //Use tex2D function to read the image
-    cudaBindTexture2D(NULL,tex8u,GPU_input,width,height,gpu_image_pitch);
+    cudaBindTexture2D(NULL,tex8u,GPU_input,width,block[numBlock].linhas,gpu_image_pitch);
 
     /*
      * Set the behavior of tex2D for out-of-range image reads.
@@ -129,7 +129,7 @@ void box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMBlock* 
     grid_size.y = (height + block_size.y - 1)/block_size.y; /*< Greater than or equal to image height */
 
     //Launch the kernel
-    box_filter_kernel_8u_c1<<<grid_size,block_size>>>(GPU_output,width,height,gpu_image_pitch,block[numBlock].lf,block[numBlock].li);
+    box_filter_kernel_8u_c1<<<grid_size,block_size>>>(GPU_output,width,imageParams->linha,gpu_image_pitch,block[numBlock].lf,block[numBlock].li);
 
     //Copy the results back to CPU
     cudaMemcpy2D(CPUoutput,widthStep,GPU_output,gpu_image_pitch,width,height,cudaMemcpyDeviceToHost);
