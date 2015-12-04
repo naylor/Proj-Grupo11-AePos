9f21582c1dc96791383e6366acc0f65ec00219ac
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 16:32:19 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -89,6 +89,7 @@ float box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams,
     unsigned char CPUinput[linhasIn];
     unsigned char CPUoutput[width*height];
 
+        printf("Apply Smooth[%d]\n", numThread);
 
     if (strcmp(imageParams->tipo, "P6")==0) {
         if (filtro == 1)
@@ -107,7 +108,6 @@ float box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams,
             CPUinput[t] = thread[numThread].pgmIn[t].gray;
     }
 
-        printf("Apply Smooth[%d]\n", numThread);
 
     //Declare GPU pointer
     unsigned char *GPU_input, *GPU_output;
