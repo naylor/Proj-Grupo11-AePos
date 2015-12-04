0292dab3ec7de2b83cd48f46d5fa033511a5252f
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 13:20:29 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -34,7 +34,7 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
         //Sum the window pixels
         for(int l2= -2; l2<=2; l2++)
         {
-            for(int c2=-2; c2<=2; c2+=3)
+            for(int c2=-2; c2<=2; c2++)
             {
             if(l2 >= 0 && c2 >= 0) {
 
