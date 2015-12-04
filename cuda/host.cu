2a887836ce04c6c78cf187a7e0a537b7c44130f8
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 21:14:47 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -41,7 +41,7 @@ float box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams,
 
     if (strcmp(imageParams->tipo, "P6")==0) {
         if (filtro == 1)
-            for(int t=0; t<width*height; t++)
+            for(int t=0; t<linhasIn-100; t++)
                 CPUinput[t] = thread[numThread].ppmIn[t].red;
         if (filtro == 2)
             for(int t=0; t<linhasIn; t++)
