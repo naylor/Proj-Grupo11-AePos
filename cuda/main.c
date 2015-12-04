e5aad60ebfd4692ca21f2dd70801f6a19f3a9b9d
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 15:32:33 -0200

    Correcoes

--- cuda/main.c
+++ cuda/main.c
@@ -86,7 +86,7 @@ int main (int argc, char **argv){
     // DE BLOCOS QUE SERAO GERADOS
     PPMNode* node = (PPMNode *)malloc(sizeof(PPMNode) * numNodes);
 
-    int t,n;
+    int t,n,cont=0;
     for(n=0; n<numNodes; n++) {
         // FAZ A DIVISAO DE LINHAS
         // POR BLOCOS
@@ -110,15 +110,20 @@ int main (int argc, char **argv){
             } else
                 relogio[1].tempoF += box_filter_8u_c1(ct, imageParams, thread, t, streamSmooth, 1);
 
+            if (cont == 5) {
+                for(int c=0; c<cont; c++) {
+                    // FAZ A GRAVACAO
+                    start_timer(tempoW); //INICIA O RELOGIO
+                    writePPMPixels(ct, imageParams, thread, t-c, n);
+                    stop_timer(tempoW);
+                }
+                cont = -1;
+            }
+            cont++;
 
         }
 
-        for(t=0; t<ct->numThreads; t++) {
-            // FAZ A GRAVACAO
-            start_timer(tempoW); //INICIA O RELOGIO
-            writePPMPixels(ct, imageParams, thread, t, n);
-            stop_timer(tempoW);
-        }
+
         free(thread);
     }
 
