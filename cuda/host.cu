eee3ce2a0c6cff551224331ef89c55ffa6cb28e6
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 20:03:27 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -27,8 +27,10 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
     int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
 
+    __shared__ float sum[blockIdx.x * blockDim.x + threadIdx.x];
 
-    float output_value = 0.0f;
+
+    //float output_value = 0.0f;
     int cont = 0;
 
     // TIRANDO A BORDA DO PROCESSAMENTO
@@ -44,11 +46,14 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
             for(int c2=-2; c2<=2; c2++)
             {
             if(l2 >= 0 && c2 >= 0) {
-                output_value += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
+                sum[index] += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
                 cont++;
             }
             }
         }
+
+        // SINCRONIZANDO AS THREADS
+        __syncthreads();
 
         //Average the output value
         output_value = output_value/cont;
@@ -58,7 +63,7 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
         int index = yIndex * pitch + xIndex;
         //printf("Smooth index:%d, xIndex:%d yIndex %d lf-li %d\n",index, xIndex, yIndex, lf-li);
 
-        output[index] = static_cast<unsigned char>(output_value);
+        output[index] = static_cast<unsigned char>sum[index]/25;
 
 }
 
