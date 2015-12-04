c8b1579657024fd8ad54c151faf75ca79366ea9c
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 20:24:09 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -7,7 +7,7 @@
 
 #define BLOCK_DIM 32
 #define BLOCK_DEFAULT 512
-#include "device_functions.h"
+
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
@@ -27,7 +27,7 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
     int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
 
-    __shared__ float sum;
+    __shared__ float sum[blockIdx.x * blockDim.x + threadIdx.x];
 
 
     //float output_value = 0.0f;
@@ -36,7 +36,6 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     // TIRANDO A BORDA DO PROCESSAMENTO
     if ( yIndex > lf-li || xIndex < 2 || xIndex > width-2 || (li == 0 && yIndex < 2) || (lf==height-1 && yIndex > (lf-li)-2) )
         return;
-        int index = yIndex * pitch + xIndex;
 
     int inicio = 0;
     if (li != 0)
@@ -47,23 +46,24 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
             for(int c2=-2; c2<=2; c2++)
             {
             if(l2 >= 0 && c2 >= 0) {
-                     atomicAdd(&sum, 1.0f);
-                //sum += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
+                output_value += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
                 cont++;
             }
             }
         }
 
         // SINCRONIZANDO AS THREADS
+        __syncthreads();
 
         //Average the output value
-        //output_value = output_value/cont;
+        output_value = output_value/cont;
 
         //Write the averaged value to the output.
         //Transform 2D index to 1D index, because image is actually in linear memory
+        int index = yIndex * pitch + xIndex;
         //printf("Smooth index:%d, xIndex:%d yIndex %d lf-li %d\n",index, xIndex, yIndex, lf-li);
 
-        output[index] = sum/cont;
+        output[index] = static_cast<unsigned char>(output_value);
 
 }
 
