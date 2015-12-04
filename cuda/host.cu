bc5eaaaed5fe90684cc4056bc998c8216ea29041
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 20:27:34 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -27,9 +27,9 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
     int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
 
-    __shared__ float sum;
-    __shared__ int cont;
 
+    float output_value = 0.0f;
+    int cont = 0;
 
     // TIRANDO A BORDA DO PROCESSAMENTO
     if ( yIndex > lf-li || xIndex < 2 || xIndex > width-2 || (li == 0 && yIndex < 2) || (lf==height-1 && yIndex > (lf-li)-2) )
@@ -39,27 +39,27 @@ __global__ void box_filter_kernel_8u_c1(unsigned char* output,const int width, c
     if (li != 0)
         inicio = 2;
 
+        #pragma unroll
         for(int l2= -2; l2<=2; l2++)
         {
             for(int c2=-2; c2<=2; c2++)
             {
             if(l2 >= 0 && c2 >= 0) {
-                sum += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
+                output_value += tex2D(tex8u,inicio+ xIndex+l2,yIndex + c2);
                 cont++;
             }
             }
         }
 
-
         //Average the output value
-        sum = sum/cont;
+        output_value = output_value/cont;
 
         //Write the averaged value to the output.
         //Transform 2D index to 1D index, because image is actually in linear memory
         int index = yIndex * pitch + xIndex;
         //printf("Smooth index:%d, xIndex:%d yIndex %d lf-li %d\n",index, xIndex, yIndex, lf-li);
 
-        output[index] = static_cast<unsigned char>(sum);
+        output[index] = static_cast<unsigned char>(output_value);
 
 }
 
