11374da0fa101bbca39525cefbe1d2597c8556c2
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 13:31:32 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -77,12 +77,12 @@ float box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams, PPMThread
     int linhasIn = thread[numThread].linhasIn;
     double linhasOut = thread[numThread].linhasOut;
 
-    const int width = imageParams->coluna;
+    const int width = 3*imageParams->coluna;
     const int height = (thread[numThread].lf-thread[numThread].li)+1;
     const int widthStep = imageParams->coluna;
 
     unsigned char CPUinput[linhasIn];
-    unsigned char CPUoutput[3*width*height];
+    unsigned char CPUoutput[width*height];
 
     int t;
     if (strcmp(imageParams->tipo, "P6")==0) {
