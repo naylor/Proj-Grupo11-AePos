9b0db13aa74e4f9536c9434c2b3784d0d1a66747
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 16:39:25 -0200

    Correcoes

--- cuda/host.cu
+++ cuda/host.cu
@@ -86,10 +86,11 @@ float box_filter_8u_c1(initialParams* ct, PPMImageParams* imageParams,
     const int height = (thread[numThread].lf-thread[numThread].li)+1;
     const int widthStep = imageParams->coluna;
 
+
+    unsigned char CPUinput;
+    CPUinput = (unsigned char *)malloc(linhasIn * sizeof(unsigned char));
         printf("Apply Smooth[%d]\n", linhasIn);
     exit(1);
-    unsigned char CPUinput[linhasIn];
-
     unsigned char CPUoutput[width*height];
 
 
