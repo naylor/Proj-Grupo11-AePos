f1ae77da26f370a4d6042e2c3f20f852fb0473db
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-04 19:59:43 -0200

    Correcoes

--- cuda/main.c
+++ cuda/main.c
@@ -102,8 +102,13 @@ int main (int argc, char **argv){
             getImageThreads(ct, imageParams, thread,  t, n);
             stop_timer(tempoR);
 
-            applySmooth(ct, imageParams, thread, t, streamSmooth);
-
+            //applySmooth(ct, imageParams, thread, t, streamSmooth);
+            if (strcmp(imageParams->tipo, "P6")==0) {
+                relogio[1].tempoF += box_filter_8u_c1(ct, imageParams, thread, t, streamSmooth, 1);
+                relogio[1].tempoF += box_filter_8u_c1(ct, imageParams, thread, t, streamSmooth, 2);
+                relogio[1].tempoF += box_filter_8u_c1(ct, imageParams, thread, t, streamSmooth, 3);
+            } else
+                relogio[1].tempoF += box_filter_8u_c1(ct, imageParams, thread, t, streamSmooth, 1);
 
             // FAZ A GRAVACAO
             start_timer(tempoW); //INICIA O RELOGIO
