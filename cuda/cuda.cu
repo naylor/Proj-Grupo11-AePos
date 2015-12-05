9ccfb0554bc2eb927d808f2a3f5e525894d6008f
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-05 02:18:41 -0200

    Correcoes

--- cuda/cuda.cu
+++ cuda/cuda.cu
@@ -18,37 +18,44 @@ texture<unsigned char, cudaTextureType2D> textureIn;
 
 
 //Box Filter Kernel For Gray scale image with 8bit depth
-__global__ void kernelTexture(unsigned char* kOutput,const int coluna, const int linha,
-                              const size_t pitch, const int lf, const int li) {
+__global__ void kernelTexture(unsigned char* output,const int width, const int height, const size_t pitch, const int lf, const int li)
+{
 
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
 
-    float sum = 0.0f;
+
+    float output_value = 0.0f;
     int cont = 0;
 
     // TIRANDO A BORDA DO PROCESSAMENTO
-    if ( y > lf-li || x < 2 || x > coluna-2 || (li == 0 && y < 2) || (lf==linha-1 && y > (lf-li)-2) )
+    if ( yIndex > lf-li || xIndex < 2 || xIndex > width-2 || (li == 0 && yIndex < 2) || (lf==height-1 && yIndex > (lf-li)-2) )
         return;
-
-
 
     int inicio = 0;
     if (li != 0)
         inicio = 2;
 
-    for(int l2= -2; l2<=2; l2++) {
-        for(int c2=-2; c2<=2; c2++) {
+        for(int l2= -2; l2<=2; l2++)
+        {
+            for(int c2=-2; c2<=2; c2++)
+            {
             if(l2 >= 0 && c2 >= 0) {
-                sum += tex2D(textureIn, inicio+x+l2, y+c2);
+                output_value += tex2D(textureIn,inicio+ xIndex+l2,yIndex + c2);
                 cont++;
             }
+            }
         }
-    }
 
-    sum = sum/cont;
+        //Average the output value
+        output_value = output_value/cont;
 
-    kOutput[y * pitch + x] = static_cast<unsigned char>(sum/cont);
+        //Write the averaged value to the output.
+        //Transform 2D index to 1D index, because image is actually in linear memory
+        int index = yIndex * pitch + xIndex;
+        //printf("Smooth index:%d, xIndex:%d yIndex %d lf-li %d\n",index, xIndex, yIndex, lf-li);
+
+        output[index] = static_cast<unsigned char>(output_value);
 
 }
 
