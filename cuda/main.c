e352aa7c4faed3e2e2c504ed8d2c6b6b9935c747
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-06 12:56:25 -0200

    Correcoes

--- cuda/main.c
+++ cuda/main.c
@@ -40,18 +40,16 @@ int main (int argc, char **argv){
 
     // DEFINE A QUANTIDADE DE LINHAS
     // DA IMAGEM PARA LEITURA E SMOOTH
-    int numMaxLinGrids = (imageParams->coluna / 512)+1;
-    numMaxLinGrids = 65535/numMaxLinGrids;
-
-        printf("\nE %d %d %d", numMaxLinGrids, imageParams->coluna, (imageParams->coluna / 512)+1 );
-
     int numMaxLinhas = imageParams->linha;
 
+    if (strcmp(imageParams->tipo, "P6")==0) {
+        int numMaxLinGrids = (imageParams->coluna / 512)+1;
+        numMaxLinGrids = 65535/numMaxLinGrids;
 
-    if (numMaxLinhas > numMaxLinGrids)
-        numMaxLinhas = numMaxLinGrids;
 
-
+        if (numMaxLinhas > numMaxLinGrids)
+            numMaxLinhas = numMaxLinGrids;
+    }
 
     ct->numThreads = 1;
 
