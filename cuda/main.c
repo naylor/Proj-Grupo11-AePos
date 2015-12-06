b2aa71f6c427eb1f1107547aad4f227aa6bc8a7f
Naylor Garcia <naylorgarcia@gmail.com>
2015-12-06 12:49:26 -0200

    Correcoes

--- cuda/main.c
+++ cuda/main.c
@@ -42,6 +42,8 @@ int main (int argc, char **argv){
     // DA IMAGEM PARA LEITURA E SMOOTH
     int numMaxLinGrids = (unsigned int) ceil((double)(65535 / (imageParams->coluna / 512)));
 
+        printf("\nE %d", numMaxLinGrids);
+
     int numMaxLinhas = imageParams->linha;
 
 
